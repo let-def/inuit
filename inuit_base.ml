e5235f88debf456ddb0fb563697c0c104e01228b
Frederic Bour <frederic.bour@lakaban.net>
2016-05-19 13:03:15 +0200

    WIP New remote patching
    TODO: simplify code by creating a global region (point-min)-(point-max) containing everything
    TODO: Edit widget should treat patches properly

--- inuit_base.ml
+++ inuit_base.ml
@@ -148,29 +148,60 @@ struct
     else
       Some parent
 
-  let enclosing_before trope offset = (
-    match Trope.find_before trope offset with None -> None | Some cursor ->
-      let lazy region = Trope.content cursor in
-      if region.left == cursor then
-        Some region
-      else (
-        assert (region.right == cursor);
-        region_parent region
+  let region_before cursor =
+    let lazy region = Trope.content cursor in
+    if region.right == cursor then
+      Some region
+    else
+      region_parent region
+
+  let region_after cursor =
+    let lazy region = Trope.content cursor in
+    if region.left == cursor then
+      Some region
+    else
+      region_parent region
+
+  let rec look_for_empty trope position cursor0 = (
+    match Trope.cursor_before trope cursor0 with
+    | Some cursor when Trope.position trope cursor = position -> (
+        let lazy region = Trope.content cursor in
+        if region.right == cursor0 then
+          Some cursor
+        else
+          look_for_empty trope position cursor
       )
+    | _ -> None
   )
 
-  let enclosing_after trope offset = (
-    match Trope.find_after trope offset with None -> None | Some cursor ->
-      let lazy region = Trope.content cursor in
-      if region.right == cursor then
-        Some region
-      else (
-        assert (region.left == cursor);
-        region_parent region
-      )
+  let insertion_cursor trope position = (
+    match Trope.find_before trope position with
+    | None -> (position, None)
+    | Some cursor0 ->
+      match position - Trope.position trope cursor0 with
+      | n when n < 0 -> assert false
+      | 0 -> (
+          match look_for_empty trope position cursor0 with
+          | Some cursor -> (0, Some cursor)
+          | None ->
+            let lazy region = Trope.content cursor0 in
+            if region.left == cursor0 then
+              (0, Some cursor0)
+            else match Trope.cursor_before trope cursor0 with
+              | None -> (position, None)
+              | Some cursor ->
+                (position - Trope.position trope cursor, Some cursor)
+        )
+      | n -> (n, Some cursor0)
   )
 
-  let regions_ancestor l r = (
+  let replacement_bound trope position = (
+    match Trope.find_after trope position with
+    | None -> None
+    | Some cursor -> Some (Trope.position trope cursor - position, cursor)
+  )
+
+  let ancestor_region l r = (
     let rec aux l r =
       let c = Trope.compare l.left r.left in
       if c < 0 then
@@ -188,31 +219,76 @@ struct
   )
 
   let remote_replace b patch = (
+    let trope = b.trope in
     let {Patch. old_len; new_len; offset; _} = patch in
-    let left = enclosing_before b.trope offset in
-    let right = enclosing_after b.trope (offset + old_len) in
-    let ancestor = match left, right with
+    (* Find bounds *)
+    let left_offset, left_cursor = insertion_cursor trope offset in
+    let right_bound = replacement_bound trope (offset + old_len) in
+    (* Find affected regions and ancestor *)
+    let left_region =
+      match left_cursor with None -> None | Some c -> region_after c in
+    let right_region =
+      match right_bound with None -> None | Some (_,c) -> region_before c in
+    let ancestor = match left_region, right_region with
       | None, _ | _, None -> None
-      | Some l, Some r -> regions_ancestor l r
+      | Some l, Some r -> ancestor_region l r
     in
-    (* Notify left branch *)
-    let left_o = match left with
+    (* Notify observers *)
+    let left_o = match left_region with
       | None -> []
-      | Some left -> notify_observers b `remote left ~stop_at:[] patch
-    in
-    (* Notify right branch *)
-    let right_o = match right with
-      | None -> []
-      | Some right ->
-        let stop_at = match ancestor with
-          | None -> []
-          | Some region -> region.observers
-        in
-        notify_observers b `remote right ~stop_at patch
+      | Some region ->
+        notify_observers b `remote region ~stop_at:[] patch
+    and right_o = match right_region with
+        | None -> []
+        | Some right ->
+          let stop_at = match ancestor with
+            | None -> []
+            | Some region -> region.observers
+          in
+          notify_observers b `remote right ~stop_at patch
     in
     (* Update trope *)
-    let trope = Trope.remove ~at:offset ~len:old_len b.trope in
-    let trope = Trope.insert trope ~at:offset ~len:new_len in
+    let trope =
+      let trope = match left_cursor, right_bound with
+        | Some l, Some (_,r) ->
+          Trope.remove_between trope l r
+        | Some l, None ->
+          Trope.remove_after trope l (left_offset + old_len)
+        | None, Some (right_offset,r) ->
+          Trope.remove_before trope r (left_offset + old_len + right_offset)
+        | None, None ->
+          Trope.remove trope ~at:0 ~len:(left_offset + old_len)
+      in
+      (* Reinsert cursors *)
+      let check = match ancestor with
+        | None -> (fun _ -> true)
+        | Some region -> (!=) region
+      in
+      let rec reinsert_from_left trope = function
+        | Some region when check region ->
+          reinsert_from_left
+            (Trope.put_back trope region.right) (region_parent region)
+        | _ -> trope
+      in
+      let rec reinsert_from_right trope = function
+        | Some region when check region ->
+          reinsert_from_right
+            (Trope.put_back trope region.left) (region_parent region)
+        | _ -> trope
+      in
+      let trope = reinsert_from_left trope left_region in
+      let trope = reinsert_from_right trope right_region in
+      (* Fix padding *)
+      let trope = match right_bound with
+        | None -> trope
+        | Some (offset, r) -> Trope.insert_before trope r offset
+      in
+      let trope = match left_cursor with
+        | None -> Trope.insert trope ~at:0 ~len:(left_offset + new_len)
+        | Some c -> Trope.insert_after trope c (left_offset + new_len)
+      in
+      trope
+    in
     b.trope <- trope;
     exec_observed right_o;
     exec_observed left_o;
@@ -220,14 +296,24 @@ struct
 
   let remote_insert b patch = (
     let {Patch. new_len; offset; _} = patch in
-    let observed = match enclosing_before b.trope offset with
-      | None -> []
-      | Some region -> notify_observers b `remote region ~stop_at:[] patch
+    let trope = b.trope in
+    let left_offset, left_cursor = insertion_cursor trope offset in
+    let left_region = match left_cursor with
+      | None -> None
+      | Some cursor -> region_after cursor
     in
-    b.trope <- Trope.insert ~at:offset ~len:new_len b.trope;
+    let trope = match left_cursor with
+      | None -> Trope.insert trope ~at:left_offset ~len:new_len
+      | Some cursor -> Trope.insert_after trope cursor new_len
+    in
+    let observed = match left_region with
+      | None -> []
+      | Some region ->
+        notify_observers b `remote region ~stop_at:[] patch
+    in
+    b.trope <- trope;
     exec_observed observed
   )
-
 
   let remote_change b patch = (
     match b.status with
